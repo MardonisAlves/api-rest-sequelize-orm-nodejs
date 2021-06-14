1a52775d0f87d0327608176814d1da167989556f
Mardonis Aves Bezerra <mardonisgp@gmail.com>
2021-06-14 09:14:42 -0300

    production

--- models/index.js
+++ models/index.js
@@ -4,8 +4,8 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
-const env =  'production';
-const config = require(__dirname + '/../config/config.json')[process.env.MODE_PROD];
+const env = process.env.MODE_PROD || 'development';
+const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 let sequelize;
