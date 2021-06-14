049f7cd9800e505e742e35b025b88bb81822a3de
Mardonis Aves Bezerra <mardonisgp@gmail.com>
2021-06-14 20:37:16 -0300

    connect

--- models/index.js
+++ models/index.js
@@ -4,10 +4,9 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
-const env =  'production';
+const env = process.env.NODE_ENV || 'production';
 console.log(env)
 const config = require(__dirname + '/../config/config.json')[env];
-//const config = require(__dirname + '/../config/env')
 console.log(config)
 const db = {};
 
@@ -15,9 +14,7 @@ let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(
-    'postgres://fbnhicgjjdxegf:26b63e12d27730401cab6ddca89fbb979a98aebca892c6d70fcc6d2223323d61@ec2-3-214-136-47.compute-1.amazonaws.com:5432/d6kki66bd8l4cr', 
-    {dialect: 'postgres'});
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
 fs
